b25d180e62364a238a0e44b50a786a7ec6c9ae3d
Luis <76595940+Luis-st@users.noreply.github.com>
2022-08-03 18:27:39 +0200

    add second client run

---
+++ src/main/java/net/luis/xsurvive/event/client/OnScreenInitEvent.java
@@ -0,0 +1,32 @@
+package net.luis.xsurvive.event.client;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+
+import net.luis.xsurvive.XSurvive;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.screens.reporting.ChatReportScreen;
+import net.minecraft.network.chat.Component;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ScreenEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber(modid = XSurvive.MOD_ID, value = Dist.CLIENT)
+public class OnScreenInitEvent {
+	
+	@SubscribeEvent
+	public static void screenInit(ScreenEvent.Init.Post event) {
+		if (event.getScreen() instanceof ChatReportScreen screen) {
+			Minecraft minecraft = screen.getMinecraft();
+			screen.sendButton = new Button((screen.width / 2) + 10, Math.min((screen.height + 300) / 2, screen.height)  - 20 - 10, 120, 20, Component.translatable("gui.chatReport.send"), (button) -> {
+				Component component = Component.literal("Due to the chat reporting system not working properly and many players being banned for no reason, this system will be disabled pending a Mojang overhaul.").withStyle(ChatFormatting.RED);
+				minecraft.player.sendSystemMessage(component);
+			}, (Button button, PoseStack stack, int mouseX, int mouseY) -> {
+				screen.renderTooltip(stack, Component.literal("Disabled for several reasons").withStyle(ChatFormatting.RED), mouseX, mouseY);
+			});
+		}
+	}
+	
+}
---
+++ src/main/java/net/luis/xsurvive/mixin/client/ChatReportScreenMixin.java
@@ -0,0 +1,26 @@
+package net.luis.xsurvive.mixin.client;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.reporting.ChatReportScreen;
+import net.minecraft.network.chat.Component;
+
+@Mixin(value = ChatReportScreen.class, priority = Integer.MIN_VALUE)
+public abstract class ChatReportScreenMixin extends Screen {
+	
+	private ChatReportScreenMixin(Component component) {
+		super(component);
+	}
+	
+	@Inject(method = "sendReport", at = @At("HEAD"))
+	private void sendReport(CallbackInfo callback) {
+		Component component = Component.literal("Due to the chat reporting system not working properly and many players being banned for no reason, this system will be disabled pending a Mojang overhaul.").withStyle(ChatFormatting.RED);
+		this.minecraft.player.sendSystemMessage(component);
+	}
+
+}
