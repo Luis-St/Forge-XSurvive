896285ff052dac4955d4594852f5ff1884fdf39c
Luis <76595940+Luis-st@users.noreply.github.com>
2022-08-08 13:17:37 +0200

    added lava source creation in the nether

---
+++ src/main/java/net/luis/xsurvive/event/level/OnCreateFluidSourceEvent.java
@@ -0,0 +1,20 @@
+package net.luis.xsurvive.event.level;
+
+import net.luis.xsurvive.XSurvive;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.level.BlockEvent.CreateFluidSourceEvent;
+import net.minecraftforge.eventbus.api.Event.Result;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber(modid = XSurvive.MOD_ID)
+public class OnCreateFluidSourceEvent {
+	
+	@SubscribeEvent
+	public static void createFluidSource(CreateFluidSourceEvent event) {
+		if (event.getLevel() instanceof Level level && level.dimension().equals(Level.NETHER)) {
+			event.setResult(Result.ALLOW);
+		}
+	}
+	
+}
