e71c862e7fe6d3746c365c07c4d044021014c0ea
Luis <76595940+Luis-st@users.noreply.github.com>
2022-08-11 15:38:56 +0200

    added WeightCollection test

--- src/main/java/net/luis/xsurvive/data/provider/loot/XSurviveGlobalLootModifierProvider.java
+++ src/main/java/net/luis/xsurvive/data/provider/loot/XSurviveGlobalLootModifierProvider.java
@@ -1,20 +1,15 @@
 package net.luis.xsurvive.data.provider.loot;
 
-import net.luis.xores.world.level.storage.loot.SmeltingModifier;
+import java.io.IOException;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.JsonOps;
+
 import net.luis.xsurvive.XSurvive;
-import net.luis.xsurvive.world.item.enchantment.XSurviveEnchantments;
-import net.luis.xsurvive.world.level.storage.loot.DiamondAppleModifier;
-import net.luis.xsurvive.world.level.storage.loot.GoldenBookModifier;
-import net.luis.xsurvive.world.level.storage.loot.MultiDropModifier;
-import net.luis.xsurvive.world.level.storage.loot.RuneItemModifier;
-import net.luis.xsurvive.world.level.storage.loot.predicates.LootTableIdsCondition;
-import net.minecraft.advancements.critereon.EnchantmentPredicate;
-import net.minecraft.advancements.critereon.ItemPredicate;
-import net.minecraft.advancements.critereon.MinMaxBounds;
+import net.luis.xsurvive.util.WeightCollection;
+import net.minecraft.data.CachedOutput;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
-import net.minecraft.world.level.storage.loot.predicates.MatchTool;
+import net.minecraft.data.DataProvider;
 import net.minecraftforge.common.data.GlobalLootModifierProvider;
 
 /**
@@ -24,35 +19,56 @@ import net.minecraftforge.common.data.GlobalLootModifierProvider;
  */
 
 public class XSurviveGlobalLootModifierProvider extends GlobalLootModifierProvider {
-
+	
+	private final DataGenerator generator;
+	
 	public XSurviveGlobalLootModifierProvider(DataGenerator generator) {
 		super(generator, XSurvive.MOD_ID);
+		this.generator = generator;
 	}
-
+	
+	@Override
+	public void run(CachedOutput cache) throws IOException {
+		WeightCollection<String> collection = new WeightCollection<>();
+		collection.add(100, "Test Entry 1");
+		collection.add(50, "Test Entry 2");
+		collection.add(25, "Test Entry 3");
+		DataProvider.saveStable(cache, WeightCollection.codec(Codec.STRING).encodeStart(JsonOps.INSTANCE, collection).getOrThrow(false, s -> {}), this.generator.getOutputFolder().resolve("test.json"));
+	}
+	
 	@Override
 	protected void start() {
-		this.add("multi_drop_modifier", new MultiDropModifier(new LootItemCondition[] {
-				new MatchTool(ItemPredicate.ANY)
-		}));
-		this.add("smelting_modifier", new SmeltingModifier(new LootItemCondition[] {
-				new MatchTool(ItemPredicate.Builder.item().hasEnchantment(new EnchantmentPredicate(XSurviveEnchantments.SMELTING.get(), MinMaxBounds.Ints.atLeast(1))).build())
-		}));
-		this.add("rune_item_modifier", new RuneItemModifier(new LootItemCondition[] {
-				new LootTableIdsCondition.Builder("chests/stronghold_library").add("minecraft:chests/stronghold_crossing").add("minecraft:chests/stronghold_corridor").add("minecraft:chests/bastion_bridge").add("minecraft:chests/bastion_hoglin_stable")
-					.add("minecraft:chests/bastion_other").add("minecraft:chests/bastion_treasure").add("minecraft:chests/end_city_treasure").add("minecraft:chests/ancient_city").add("minecraft:chests/ancient_city_ice_box").build()
-		}));
-		this.add("golden_book_modifier", new GoldenBookModifier(new LootItemCondition[] {
-				new LootTableIdsCondition.Builder(new ResourceLocation("chests/stronghold_library")).add("minecraft:chests/bastion_treasure").add("minecraft:chests/end_city_treasure").add("minecraft:chests/ancient_city")
-					.add("minecraft:chests/ancient_city_ice_box").build()
-		}));
-		this.add("diamond_apple_modifier", new DiamondAppleModifier(new LootItemCondition[] {
-				
-		}));
+		
+		
+		
+		
+		
+//		this.add("multi_drop_modifier", new MultiDropModifier(new LootItemCondition[] {
+//			new MatchTool(ItemPredicate.ANY)
+//		}));
+//		this.add("smelting_modifier", new SmeltingModifier(new LootItemCondition[] {
+//			new MatchTool(ItemPredicate.Builder.item().hasEnchantment(new EnchantmentPredicate(XSurviveEnchantments.SMELTING.get(), MinMaxBounds.Ints.atLeast(1))).build())
+//		}));
+//		this.add("rune_item_modifier", new RuneItemModifier(new LootItemCondition[] {
+//			new LootTableIdsCondition.Builder("chests/stronghold_library").add("chests/stronghold_crossing").add("chests/stronghold_corridor").add("chests/bastion_bridge").add("chests/bastion_hoglin_stable").add("chests/bastion_other")
+//				.add("chests/bastion_treasure").add("chests/end_city_treasure").add("chests/ancient_city").add("chests/ancient_city_ice_box").build()
+//		}));
+//		// Not final solution
+//		this.add("golden_book_modifier", new GoldenBookModifier(new LootItemCondition[] {
+//			new LootTableIdsCondition.Builder("chests/stronghold_library").add("chests/bastion_treasure").add("chests/end_city_treasure").add("chests/ancient_city").add("chests/ancient_city_ice_box").build()
+//		}, Util.make(new WeightCollection<>(), (collection) -> {
+//			collection.add(50, EnchantmentList.of(EnchantmentRarity.COMMON, LootModifierHelper.getCommonEnchantments()));
+//			collection.add(25, EnchantmentList.of(EnchantmentRarity.RARE, LootModifierHelper.getRareEnchantments()));
+//			collection.add(15, EnchantmentList.of(EnchantmentRarity.VERY_RARE, LootModifierHelper.getVeryRareEnchantments()));
+//			collection.add(10, EnchantmentList.of(EnchantmentRarity.TREASURE, LootModifierHelper.getTreasureEnchantments()));
+//		}), EnchantmentList.of(EnchantmentRarity.TREASURE, LootModifierHelper.getNetherTreasureEnchantments()), EnchantmentList.of(EnchantmentRarity.TREASURE, LootModifierHelper.getEndTreasureEnchantments())));
+//		this.add("diamond_apple_modifier", new AdditionalChanceItemModifier(new LootItemCondition[] {}, Items.GOLDEN_APPLE, XSurviveItems.DIAMOND_APPLE.get(), Chance.of(0.07)));
+//		this.add("enchanted_diamond_apple_modifier", new AdditionalChanceItemModifier(new LootItemCondition[] {}, Items.ENCHANTED_GOLDEN_APPLE, XSurviveItems.ENCHANTED_DIAMOND_APPLE.get(), Chance.of(0.03)));
 	}
 	
 	@Override
 	public String getName() {
 		return "XSurvive Global Loot Modifiers";
 	}
-
+	
 }
