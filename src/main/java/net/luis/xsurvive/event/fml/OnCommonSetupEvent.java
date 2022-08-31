42ac795e57eca2afb8f576571a90eae70be0f2b4
Luis <76595940+Luis-st@users.noreply.github.com>
2022-08-31 20:27:54 +0200

    fixed incorrect displaying of custom Brewing Recipes in Jei

--- src/main/java/net/luis/xsurvive/event/fml/OnCommonSetupEvent.java
+++ src/main/java/net/luis/xsurvive/event/fml/OnCommonSetupEvent.java
@@ -2,7 +2,7 @@ package net.luis.xsurvive.event.fml;
 
 import net.luis.xsurvive.XSurvive;
 import net.luis.xsurvive.dependency.DependencyCallWrapper;
-import net.luis.xsurvive.world.item.alchemy.XSBrewingRecipe;
+import net.luis.xsurvive.world.item.alchemy.BrewingRecipeUtils;
 import net.luis.xsurvive.world.item.alchemy.XSPotions;
 import net.luis.xsurvive.world.level.entity.ai.custom.CustomAiManager;
 import net.luis.xsurvive.world.level.entity.ai.custom.CustomElderGuardianAi;
@@ -12,7 +12,6 @@ import net.minecraft.world.entity.monster.ElderGuardian;
 import net.minecraft.world.entity.monster.EnderMan;
 import net.minecraft.world.entity.monster.ZombifiedPiglin;
 import net.minecraft.world.item.Items;
-import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
@@ -35,9 +34,9 @@ public class OnCommonSetupEvent {
 	}
 	
 	private static void registerBrewingRecipies() {
-		BrewingRecipeRegistry.addRecipe(new XSBrewingRecipe.Builder(Items.SNOWBALL, XSPotions.FORST.get()).useDefaultInput().addTimeUpgrade(XSPotions.LONG_FORST.get()).addPowerUpgrade(XSPotions.STRONG_FORST.get()).build());
-		BrewingRecipeRegistry.addRecipe(new XSBrewingRecipe.Builder(Items.WITHER_ROSE, XSPotions.WITHER.get()).useDefaultInput().addTimeUpgrade(XSPotions.LONG_WITHER.get()).addPowerUpgrade(XSPotions.STRONG_WITHER.get()).build());
-		BrewingRecipeRegistry.addRecipe(new XSBrewingRecipe.Builder(Items.IRON_PICKAXE, XSPotions.DIG_SPEED.get()).useDefaultInput().addTimeUpgrade(XSPotions.LONG_DIG_SPEED.get()).addPowerUpgrade(XSPotions.STRONG_DIG_SPEED.get()).build());
+		BrewingRecipeUtils.addRecipes(Items.SNOWBALL, XSPotions.FORST.get(), XSPotions.LONG_FORST.get(), XSPotions.STRONG_FORST.get());
+		BrewingRecipeUtils.addRecipes(Items.WITHER_ROSE, XSPotions.WITHER.get(), XSPotions.LONG_WITHER.get(), XSPotions.STRONG_WITHER.get());
+		BrewingRecipeUtils.addRecipes(Items.IRON_PICKAXE, XSPotions.DIG_SPEED.get(), XSPotions.LONG_DIG_SPEED.get(), XSPotions.STRONG_DIG_SPEED.get());
 	}
 	
 	private static void registerCustomAis() {
---
+++ src/main/java/net/luis/xsurvive/world/item/alchemy/BrewingRecipeUtils.java
@@ -0,0 +1,38 @@
+package net.luis.xsurvive.world.item.alchemy;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.Potion;
+import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.item.alchemy.Potions;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
+
+/**
+ *
+ * @author Luis-st
+ *
+ */
+
+public class BrewingRecipeUtils {
+	
+	public static void addRecipes(Item baseItem, Potion potion, Potion longPotion, Potion strongPotion) {
+		simpleRecipe(baseItem, potion);
+		longRecipe(potion, longPotion);
+		strongRecipe(potion, strongPotion);
+	}
+	
+	public static void simpleRecipe(Item baseItem, Potion resultPotion) {
+		BrewingRecipeRegistry.addRecipe(Ingredient.of(PotionUtils.setPotion(new ItemStack(Items.POTION), Potions.AWKWARD)), Ingredient.of(baseItem), PotionUtils.setPotion(new ItemStack(Items.POTION), resultPotion));
+	}
+	
+	public static void longRecipe(Potion inputPotion, Potion resultPotion) {
+		BrewingRecipeRegistry.addRecipe(Ingredient.of(PotionUtils.setPotion(new ItemStack(Items.POTION), inputPotion)), Ingredient.of(Items.REDSTONE), PotionUtils.setPotion(new ItemStack(Items.POTION), resultPotion));
+	}
+	
+	public static void strongRecipe(Potion inputPotion, Potion resultPotion) {
+		BrewingRecipeRegistry.addRecipe(Ingredient.of(PotionUtils.setPotion(new ItemStack(Items.POTION), inputPotion)), Ingredient.of(Items.GLOWSTONE_DUST), PotionUtils.setPotion(new ItemStack(Items.POTION), resultPotion));
+	}
+	
+}
