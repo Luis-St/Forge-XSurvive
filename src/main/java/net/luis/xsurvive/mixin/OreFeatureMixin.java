ebf08ab579351807d1b7db1b35e48b69ab580d5d
Luis <76595940+Luis-st@users.noreply.github.com>
2022-09-20 20:55:56 +0200

    fixed ore air exposure includes fluids

---
+++ src/main/java/net/luis/xsurvive/mixin/OreFeatureMixin.java
@@ -0,0 +1,54 @@
+package net.luis.xsurvive.mixin;
+
+import java.util.function.Function;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.mojang.serialization.Codec;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.BlockPos.MutableBlockPos;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.LiquidBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.OreFeature;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration.TargetBlockState;
+
+/**
+ *
+ * @author Luis-st
+ *
+ */
+
+@Mixin(OreFeature.class)
+public abstract class OreFeatureMixin extends Feature<OreConfiguration> {
+	
+	private OreFeatureMixin(Codec<OreConfiguration> codec) {
+		super(codec);
+	}
+	
+	@Shadow
+	private static boolean shouldSkipAirCheck(RandomSource rng, float chance) {
+		return false;
+	}
+	
+	@Inject(method = "canPlaceOre", at = @At("HEAD"), cancellable = true)
+	private static void canPlaceOre(BlockState blockState, Function<BlockPos, BlockState> function, RandomSource rng, OreConfiguration config, TargetBlockState targetState, MutableBlockPos pos, CallbackInfoReturnable<Boolean> callback) {
+		if (!targetState.target.test(blockState, rng)) {
+			callback.setReturnValue(false);
+		} else if (shouldSkipAirCheck(rng, config.discardChanceOnAirExposure)) {
+			callback.setReturnValue(true);
+		} else {
+			callback.setReturnValue(!checkNeighbors(function, pos.immutable(), (state) -> {
+				return state.isAir() || state.getBlock() instanceof LiquidBlock;
+			}));
+		}
+	}
+	
+}
