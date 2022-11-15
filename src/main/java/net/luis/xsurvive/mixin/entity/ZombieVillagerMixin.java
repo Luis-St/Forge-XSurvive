42216e657c0301a41da8ad7a2f441c5a018caafc
Luis <76595940+Luis-st@users.noreply.github.com>
2022-11-15 21:26:43 +0100

    changed converting Item of ZombieVillager to EnchantedGoldenApple

---
+++ src/main/java/net/luis/xsurvive/mixin/entity/ZombieVillagerMixin.java
@@ -0,0 +1,58 @@
+package net.luis.xsurvive.mixin.entity;
+
+import java.util.UUID;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.monster.Zombie;
+import net.minecraft.world.entity.monster.ZombieVillager;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+
+/**
+ *
+ * @author Luis-st
+ *
+ */
+
+@Mixin(ZombieVillager.class)
+public abstract class ZombieVillagerMixin extends Zombie {
+	
+	private ZombieVillagerMixin(EntityType<? extends Zombie> entityType, Level level) {
+		super(entityType, level);
+	}
+	
+	@Shadow
+	protected abstract void startConverting(UUID uuid, int time);
+	
+	@Inject(method = "mobInteract", at = @At("HEAD"), cancellable = true)
+	public void mobInteract(Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResult> callback) {
+		ItemStack stack = player.getItemInHand(hand);
+		if (stack.is(Items.ENCHANTED_GOLDEN_APPLE)) {
+			if (this.hasEffect(MobEffects.WEAKNESS)) {
+				if (!player.getAbilities().instabuild) {
+					stack.shrink(1);
+				}
+				if (!this.level.isClientSide) {
+					this.startConverting(player.getUUID(), this.random.nextInt(2401) + 3600);
+				}
+				callback.setReturnValue(InteractionResult.SUCCESS);
+			} else {
+				callback.setReturnValue(InteractionResult.CONSUME);
+			}
+		} else {
+			callback.setReturnValue(super.mobInteract(player, hand));
+		}
+	}
+	
+}
